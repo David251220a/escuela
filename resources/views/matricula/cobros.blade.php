8042bc26de4946baee7b4a43e3a029217b28af8b
David Ortiz <davidortiz25122010@gmail.com>
2022-06-12 14:49:28 -0400

    cambio

--- resources/views/matricula/cobros.blade.php
+++ resources/views/matricula/cobros.blade.php
@@ -209,7 +209,6 @@
         </div>
 
         <div class="hidden p-1 rounded-lg" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
-            2
             <div class="flex flex-col mb-4">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
